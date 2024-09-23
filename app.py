11ff989bf6014cf2cd5aa877f750aa2a36b1e248
pinilDissanayaka <pinildissanayaka@gmail.com>
2024-09-23 17:56:50 +0530

    define technology var

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import streamlit as st
 from tempfile import TemporaryDirectory
 from utils.repo_handler import clone_github_repo, load_repo, summary_loaded_document
 
+used_technologies=""
 
 with st.sidebar:
     st.write("This code will be printed to the sidebar.")
